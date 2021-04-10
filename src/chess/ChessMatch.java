715bfdd5b374dc10f721c41c868d13954360f85f
Victor <vrvmaciel@gmail.com>
2021-04-10 17:51:48 -0300

    Adicionando excessoes ao systema

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +10,7 @@ public class ChessMatch {
 
     public ChessMatch() {
         board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -20,6 +23,16 @@ public class ChessMatch {
         }
         return mat;
 
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+
+    }
+
+    private void initialSetup(){
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.WHITE));
     }
 
 }
